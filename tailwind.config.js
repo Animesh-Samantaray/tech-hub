dc3259f76c6fb1f2a2f221aa3ac330d688b48d75
Animesh Samantaray <animeshsamantaray2@gmail.com>
2025-06-30 21:35:25 +0530

    aaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,3 +8,5 @@ module.exports = {
   },
   plugins: [],
 }
+
+tailwind.config.js
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-// module.exports = {
-//   darkMode: 'class', // required for manual control
-//   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-//   theme: { extend: {} },
-//   plugins: [],
-// };
-
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() , tailwindcss()],
+  // You can add more config here if needed
 })
